1bb476a391eac161299ad404eefe0040e943cdb3
rigglet <39748939+rigglet@users.noreply.github.com>
2020-12-11 20:05:09 +0000

    Update time

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,4 +1,4 @@
-import React, {useRef} from "react";
+import React, {useRef, useState} from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlay, faAngleLeft, faAngleRight } from "@fortawesome/free-solid-svg-icons";
 
@@ -15,19 +15,29 @@ const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
             setIsPlaying(!isPlaying);
         }
     }
+    const timeUpdateHandler = (e) => {
+        const current = e.target.currentTime;
+        const duration = e.target.duration;
+        setSonginfo({ ...songInfo, currentTime: current, duration });
+    }
+    //state
+    const [songInfo, setSonginfo] = useState({ currentTime: null, duration: null });
+    const getTime = (time) => {
+        return Math.floor(time / 60) + ":" + ("0" + Math.floor(time % 60)).slice(-2)
+    }
     return (
         <div className="player">
             <div className="time-control">
-                <p>Start Time</p>
+                <p>{getTime(songInfo.currentTime)}</p>
                 <input type="range"></input>
-                <p>End Time</p>
+                <p>{getTime(songInfo.duration)}</p>
             </div>
             <div className="play-control">
                 <FontAwesomeIcon className="slip-back" size="2x" icon={faAngleLeft} />
                 <FontAwesomeIcon onClick={ playSongHandler} className="play" size="2x" icon={faPlay} />
                 <FontAwesomeIcon className="slip-forward" size="2x" icon={faAngleRight} />
             </div>
-            <audio ref={audioRef} src={currentSong.audio}></audio> 
+            <audio onTimeUpdate={timeUpdateHandler} onLoadedMetadata={timeUpdateHandler} ref={audioRef} src={currentSong.audio}></audio> 
         </div>
     )
 }
